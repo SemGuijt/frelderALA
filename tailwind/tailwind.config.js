1e8fe43f2c4530b5604ce27af38ead1912f8cd9d
PnthrMBO <6014350@mborijnland.nl>
2023-06-19 09:41:12 +0200

    dashboard
    dashboard toegevoegd gemaakt met tailwind

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {      colors: {
+      'card1': '#EFD2AC',
+      'card2': '#8B8C89',
+      'card3': '#606C38',
+      'card4': '#DDA15E',
+    },
+  },
+  },
+  plugins: [],
+}
+
